adebc916146b854b5111e003a5e85c41f4eb3c44
ashkan <karimiashkan8186@gmail.com>
2022-10-17 16:16:11 +0330

    add teachers and students status changed SMS

--- Teacher/api/serializers.py
+++ Teacher/api/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+
+from Sms.helpers import send_sms
+from Sms.sms_texts import SMS_TEXTS
 from ..models import *
 
 
@@ -6,3 +9,17 @@ class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = "__all__"
+
+    def save(self, **kwargs):
+        instance = super().save(**kwargs)
+
+        if instance.user and hasattr(instance.user, 'phone'):
+            if instance.is_approved:
+                sms_text = SMS_TEXTS['teacher_approved'].format(instance.user.full_name)
+                send_sms(instance.user.phone, sms_text)
+            else:
+                sms_text = SMS_TEXTS['teacher_reject'].format(instance.user.full_name)
+                send_sms(instance.user.phone, sms_text)
+
+        return instance
+
