ed131cefbf28fa5409ea939277999a4a3590073e
ashkan <karimiashkan8186@gmail.com>
2022-10-18 00:48:32 +0330

    change poll fields

--- Poll/models.py
+++ Poll/models.py
@@ -34,17 +34,22 @@ class UserPoll(CustomModel):
         on_delete=models.CASCADE,
         related_name='user_polls',
     )
-    presentation_of_the_lesson_plan = models.CharField(verbose_name='ارئه طرح درس و معرفی منابع و رفرنس های معتبر', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    mastery_of_the_teacher_on_the_subject_of_the_lesson = models.CharField(verbose_name='تسلط استاد بر موضوع درس', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    way_of_expression = models.CharField(verbose_name='تسلط استاد بر موضوع درس', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    method_of_expression = models.CharField(verbose_name='شیوه بیان، تفهیم و انتقال مطالب درسی', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    teaching_method = models.CharField(verbose_name='شیوه تدریس و بکار گیری امکانات کمک آموزشی موجود', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    participation = models.CharField(verbose_name='جلب مشارکت و ایجاد انگیزه در مباحث درسی', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    responsiveness = models.CharField(verbose_name='اهمیت دادن به پاسخگویی سوالات درسی دانشجویان', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    being_scientific = models.CharField(verbose_name='میزان علمی بودن محتوای کلاس أموزشی', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    order_and_time = models.CharField(verbose_name='نحوه مدیریت کلاس (نظم و زمان)', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    arrivals_and_departures = models.CharField(verbose_name='ورود و خروج به موقع به کلاس', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
-    social_etiquette_and_behavior = models.CharField(verbose_name='آداب و رفتار اجتماعی با دانشجویان و رفتار متقابل', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES)
+    presentation_of_the_lesson_plan = models.CharField(verbose_name='ارئه طرح درس و معرفی منابع و رفرنس های معتبر', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    mastery_of_the_teacher_on_the_subject_of_the_lesson = models.CharField(verbose_name='تسلط استاد بر موضوع درس', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    method_of_expression = models.CharField(verbose_name='شیوه بیان، تفهیم و انتقال مطالب درسی', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    teaching_method = models.CharField(verbose_name='شیوه تدریس و بکار گیری امکانات کمک آموزشی موجود', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    participation = models.CharField(verbose_name='جلب مشارکت و ایجاد انگیزه در مباحث درسی', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    responsiveness = models.CharField(verbose_name='اهمیت دادن به پاسخگویی سوالات درسی دانشجویان', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    being_scientific = models.CharField(verbose_name='میزان علمی بودن محتوای کلاس أموزشی', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    order_and_time = models.CharField(verbose_name='نحوه مدیریت کلاس (نظم و زمان)', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    arrivals_and_departures = models.CharField(verbose_name='ورود و خروج به موقع به کلاس', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    social_etiquette_and_behavior = models.CharField(verbose_name='آداب و رفتار اجتماعی با دانشجویان و احترام متقابل', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    final_evaluation = models.CharField(verbose_name='ارزیابی پایانی دانشپذیران به طرز درست و صحیح', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    projector_quality = models.CharField(verbose_name='کیفیت ویدیو پروژکتور، میز و صندلی و وایت برد', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    quality_of_the_venue = models.CharField(verbose_name='کیفیت تهویه، روشنایی و مکان برگزاری', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    perform_service = models.CharField(verbose_name='انجام خدمت مد نظر توسط کارکنان در زمان مقرر', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    way_the_staff_behaves = models.CharField(verbose_name='نحوه برخورد کارکنان', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
+    Information_and_guidance = models.CharField(verbose_name='اطلاع رسانی و راهنمایی لازم بصورت شفاف و دقیق', max_length=255, choices=POLL_QUESTION_OPTIONS.CHOICES, null=True)
 
     class Meta:
         verbose_name = 'پاسخ فرم نظرسنجی'
