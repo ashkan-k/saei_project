39505650db2351d91678e26029e936ca27a887be
ashkan <karimiashkan8181@gmail.com>
2022-10-24 16:32:58 +0330

    complete send sms helper function

--- Sms/helpers.py
+++ Sms/helpers.py
@@ -8,15 +8,7 @@ def send_sms(receiver, msg):
     response = requests.get(
         f'{settings.SMS_WEB_SERVICE_URL}username={settings.SMS_USERNAME}&password={settings.SMS_PASSWORD}&from={settings.SMS_FROM_NUMBER}&to={receiver}&text={msg}'
     )
-    response = response.json()
 
-    from django.core.mail import send_mail
-    subject = 'Thank you for registering to our site'
-    message = response
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['karimiashkan8181@gmail.com',]
-    send_mail( subject, message, email_from, recipient_list )
-
-    if response == 0:
+    if response.status_code == 200 and response.text == 'SendWasSuccessful':
         return False
     return True
