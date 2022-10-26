92f4c169f3cf7ff9ad331d12822455529d56a295
ashkan <karimiashkan8181@gmail.com>
2022-10-26 22:51:50 +0330

    fix unicode persian numbers

--- config/settings.py
+++ config/settings.py
@@ -137,12 +137,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-                'ENGINE': 'django.db.backends.mysql',
-                "NAME": config('DB_NAME'),
-                "USER": config('DB_USER'),
-                "PASSWORD": config('DB_PASSWORD'),
-                "HOST": config('DB_HOST'),
-                "PORT": config('DB_PORT'),
+                'ENGINE': 'django.db.backends.postgresql',
+                "NAME": config('POSTGRES_DB_NAME'),
+                "USER": config('POSTGRES_DB_USER'),
+                "PASSWORD": config('POSTGRES_DB_PASSWORD'),
+                "HOST": config('POSTGRES_DB_HOST'),
+                "PORT": config('POSTGRES_DB_PORT'),
     }
 }
 
