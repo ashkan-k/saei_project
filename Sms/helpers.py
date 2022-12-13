fff8a807aaff115997292ecdb1b2e43eb4f061e6
ashkan <karimiashkan8181@gmail.com>
2022-12-13 18:54:57 +0330

    fix sms not sending problem

--- Sms/helpers.py
+++ Sms/helpers.py
@@ -9,12 +9,6 @@ def send_sms(receiver, msg):
         f'{settings.SMS_WEB_SERVICE_URL}username={settings.SMS_USERNAME}&password={settings.SMS_PASSWORD}&from={settings.SMS_FROM_NUMBER}&to={receiver}&text={msg}'
     )
 
-    print('dddddddddddddddddddddddddd')
-    print(response.status_code)
-    print(response.text)
-    print(settings.SMS_USERNAME)
-    print(settings.SMS_PASSWORD)
-
     if response.status_code == 200 and response.text == 'SendWasSuccessful':
         return False
     return True
